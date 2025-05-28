3b9c5573fe0e718ae8e7157d3b300d3b370343fc
Ch4rybd3 <100271179+Ch4rybd3@users.noreply.github.com>
2025-05-28 16:50:45 +0200

    Added the find and replace functionnality

--- Kelpie/engines/builder.py
+++ Kelpie/engines/builder.py
@@ -4,15 +4,25 @@ def yaml_to_python_script(yaml_path, output_path):
     with open(yaml_path, 'r', encoding='utf-8') as f:
         data = yaml.safe_load(f)
 
-    # Les blocs dans l'ordre d'apparition dans le YAML (car dict Python >=3.7 garde l'ordre)
+    # Les valeurs à remplacer
+    replacements = {
+        "{{c2_url}}": "http://10.0.0.10/api",
+        "{{folder_path}}": "C:/temp",
+        "{{rsa_key_b64}}": "thisisasuperrsakeyinbase64trololo"
+    }
+
     with open(output_path, 'w', encoding='utf-8') as f_out:
         for key, block in data.items():
+            # Remplacement dans le bloc
+            for placeholder, real_value in replacements.items():
+                block = block.replace(placeholder, real_value)
+
             f_out.write(f"# --- {key} ---\n")
             f_out.write(block)
             f_out.write("\n\n")
 
 if __name__ == "__main__":
-    yaml_path = "Kelpie/templates/wannaswim.yml"     # ton fichier yaml
-    output_path = "Kelpie/malwares/source_code/wannaswim.py"  # script python généré
+    yaml_path = "Kelpie/templates/wannaswim.yml"
+    output_path = "Kelpie/malwares/source_code/wannaswim.py"
     yaml_to_python_script(yaml_path, output_path)
     print(f"Script Python généré dans {output_path}")
